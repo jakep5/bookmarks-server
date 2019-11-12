04f4f72bb322a956369d1540db64cf791efa1d4e
jakep5 <jakepagel1@yahoo.com>
2019-11-12 12:48:36 -0600

    add PATCH endpoint, create test file

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -75,5 +75,28 @@ bookmarksRouter
             })
             .catch(next)
     })
+    .patch(jsonParser, (req, res, next) => {
+        const {title, url, description, rating} = req.body
+        const bookmarkToUpdate = {title, url, description, rating}
+        
+        const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain either 'title', 'url', 'description', or 'rating'`
+                }
+            })
+        }
+
+        BookmarksService.updateBookmark(
+            req.app.get('db'),
+            req.params.bookmark_id,
+            bookmarkToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
 
 module.exports = bookmarksRouter
